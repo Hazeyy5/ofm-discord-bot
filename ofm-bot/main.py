247c9f5e7096c388717afa5b5f315e1dcda046cf
Théo Delaplace <contact.delaplacetheo@gmail.com>
2025-04-16 20:57:33 +0200

    Update main.py

--- ofm-bot/main.py
+++ ofm-bot/main.py
@@ -26,5 +26,7 @@ async def on_ready():
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
+        bot.load_extension("cogs.contrat")
+
 
 bot.run(os.getenv("DISCORD_TOKEN"))
